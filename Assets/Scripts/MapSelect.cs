1864dfb18b341c4eae191095bb4a46f51edbdd49
Maciej Orlik <100729493+MaciejOrlik@users.noreply.github.com>
2022-04-24 15:34:22 +0200

    fix filmów
    fix filmów

--- Assets/Scripts/MapSelect.cs
+++ Assets/Scripts/MapSelect.cs
@@ -7,8 +7,13 @@ using TMPro;
 public class MapSelect : MonoBehaviour
 {
     public TextMeshProUGUI mapName;
-    private string[] mapNames = { "1 - Countryside", "2 - Snowcircle", "3 - Dusty Castles", "4 - Big Farms" };
+    private string[] mapNames = { "Countryside", "Snowcircle", "Dusty Castles", "Big Farms" };
     private int mapIndex = 0;
+
+    public GameObject i1,i2,i3,i4;
+    public RawImage ri;
+    public Texture t1,t2,t3,t4;
+    public Image image;
 
     private void Start()
     {
@@ -20,6 +25,54 @@ public class MapSelect : MonoBehaviour
         {
             SceneManager.LoadScene("Garage");
         }
+        
+        if(mapIndex==0)
+        {
+            i1.SetActive(true);
+            i2.SetActive(false);
+            i3.SetActive(false);
+            i4.SetActive(false);
+
+            ri.texture = t1;
+            image.GetComponent<Image>().color = new Color32(48, 255, 7, 225);
+
+        }
+        else if(mapIndex == 1)
+        {
+            i2.SetActive(true);
+            i1.SetActive(false);
+            i3.SetActive(false);
+            i4.SetActive(false);
+
+            ri.texture = t2;
+
+            image.GetComponent<Image>().color = new Color32(6, 43, 225, 225);
+
+        }
+        else if (mapIndex == 2)
+        {
+            i3.SetActive(true);
+            i1.SetActive(false);
+            i2.SetActive(false);
+            i4.SetActive(false);
+
+            ri.texture = t3;
+
+            image.GetComponent<Image>().color = new Color32(255, 130, 6, 225);
+
+        }
+        else
+        {
+            i4.SetActive(true);
+            i1.SetActive(false);
+            i2.SetActive(false);
+            i3.SetActive(false);
+
+            ri.texture = t4;
+
+            image.GetComponent<Image>().color = new Color32(255, 255, 255, 225);
+
+    }
     }
 
     public void Play()
